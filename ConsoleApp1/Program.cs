4b7d23398af81d3a2794209aad34922fc70d189e
saba1122333 <tchumburidzesaba@gmail.com>
2025-05-22 17:43:37 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace MyApp
+{
+
+    internal class WebServer
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start Web Server ");
+        }
+    }
+
+}
---
+++ ConsoleApp1/Webroot/script.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    console.log('Web server page loaded!');
+
+    // Add click effect to title
+    const title = document.querySelector('.animated-title');
+    title.addEventListener('click', function () {
+        this.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    });
+});
